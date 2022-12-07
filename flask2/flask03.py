716f1e458201495b1fb56a0fcf44b541111b6c0f
Wes <whuneyc5@uncc.edu>
2022-12-07 00:59:01 -0500

    flask app #3 - added execution path for POST request of new note

--- flask2/flask03.py
+++ flask2/flask03.py
@@ -4,6 +4,7 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 app = Flask(__name__)     # create an app
 
 # @app.route is a decorator. It gives the function "index" special powers.
@@ -15,20 +16,33 @@ def index():
     return render_template("index.html", user = a_user)
 @app.route('/notes')
 def get_notes():
+    a_user = {'name': 'Wes', 'email':'whuney@uncc.edu'}
     notes = {1 : {'title': 'First note', 'text:': 'This is my first note', 'date': '10-1-2020'},
-    2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'  } }
-    return render_template('notes.html', notes=notes)
+    2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'  },
+    3: {'title' : 'Third note', 'text': 'This is my third note', 'date': '10-8-2020'} 
+     }
+    return render_template('notes.html', notes=notes, user=a_user)
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     notes ={1: {'title': 'First note', 'text': 'This is my first note', 'date':'10-1-2020'},
             2: {'title' :'Second note', 'text': 'This is my second note', 'date': '10-2-2020'},
             3: {'title' : 'Third note', 'text': 'This is my third note', 'date': '10-8-2020'} 
                 }
-    return render_template('note.html', note=notes[int(note_id)])
-@app.route('/notes/new')
+    a_user = {'name': 'Wes', 'email':'whuney@uncc.edu'}
+    return render_template('note.html', note=notes[int(note_id)], user = a_user)
+@app.route('/notes/new', methods = ['GET', 'POST'])
 def new_note():
-    a_user = {'name': 'Wes', 'email':'whuney@unccc.edu'}
-    return render_template('new.html', user=a_user)
+#MOCK USER
+    a_user = {'name': 'Wes', 'email':'whuney@uncc.edu'}
+ #check if method used for request
+    print('request method is', request.method)
+    if request.method == 'POST':
+     return '<h1> POST method used for this request </h1>'
+    else:
+     return render_template('new.html', user=a_user)
+
+    
+    
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
